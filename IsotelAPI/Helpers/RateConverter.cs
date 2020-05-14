dca184c8418178999a263b7b259a82578a57bb04
miutamihai <miuta.mihai@gmail.com>
2020-05-14 17:26:49 +0300

    added functionality for currency conversion

---
+++ IsotelAPI/Helpers/RateConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace IsotelAPI.Helpers
+{
+    public class RateConverter
+    {
+        public static double Convert(string requestedCurrency, int priceInDollars)
+        {
+            string regexPattern = @"(?<=(?:";
+            regexPattern += '"';
+            regexPattern += requestedCurrency;
+            regexPattern += '"';
+            regexPattern += @":)(.*?)(?:)).+?(?=})";
+            Regex regex = new Regex(regexPattern);
+            WebRequest request = WebRequest.Create("https://api.exchangeratesapi.io/latest?base=USD&symbols=" + requestedCurrency);
+            request.Method = "GET";
+            WebResponse response = request.GetResponse();
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(dataStream);
+                string responseFromServer = reader.ReadToEnd();
+                double rate = Convert.ToDouble(regex.Match(responseFromServer).Value);
+                return rate * priceInDollars;
+            }
+        }
+    }
+}
